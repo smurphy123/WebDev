95d7e6e598440e333265909e80905910d12dc82e
smurphy123 <smurphysmail@gmail.com>
2021-09-05 12:39:51 -0400

    Repo Update

--- 50Projects50Days/ProgressSteps/script.js
+++ 50Projects50Days/ProgressSteps/script.js
@@ -1,50 +1,49 @@
-const progess = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('circle');
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
-let currentActive = 1;
+let currentActive = 1
 
 next.addEventListener('click', () => {
-    currentActive++;
+    currentActive++
 
     if(currentActive > circles.length) {
-        currentActive = circles.length;
+        currentActive = circles.length
     }
 
+    update()
+})
 
-});
-
-next.addEventListener('click', () => {
-    currentActive--;
+prev.addEventListener('click', () => {
+    currentActive--
 
     if(currentActive < 1) {
-        currentActive = 1;
+        currentActive = 1
     }
 
-    update();
-});
+    update()
+})
 
 function update() {
     circles.forEach((circle, idx) => {
         if(idx < currentActive) {
-            circle.classList.add('active');
+            circle.classList.add('active')
         } else {
-            circle.classList.remove('active');
+            circle.classList.remove('active')
         }
     })
 
-    const actives = documents.querySelectorAll('.active');
+    const actives = document.querySelectorAll('.active')
 
-    // update blue progess line when moving to next circle
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 
-    if(currentActive === 1) { // first circle
-        prev.disabled = true;
-    } else if (currentActive === circles.length) {
-        next.disabled = true;
-    } else { // last circle
-        prev.disabled = false;
-        next.disabled = false;
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
     }
 }
